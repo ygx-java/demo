168775d84bd9503ae4aeacf32beaf7894df05db6
ygx-java <591103204@qq.com>
2019-12-19 15:50:20 +0800

    SpringBoot 集成邮件服务

--- src/main/java/com/base/rocket/cunsumer/CunsumerNote.java
+++ src/main/java/com/base/rocket/cunsumer/CunsumerNote.java
@@ -2,14 +2,21 @@ package com.base.rocket.cunsumer;
 
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RocketMQMessageListener(
-		consumerGroup = "consumerGroup 2",
-		topic = "topic-2",
-		selectorExpression = "*",
-		consumeMode = ConsumeMode.ORDERLY)
-public class CunsumerNote {
+		consumerGroup = "consumerGroup2"
+		,topic = "topic-2"
+		,selectorExpression = "*"
+//		,consumeMode = ConsumeMode.ORDERLY
+		)
+public class CunsumerNote implements RocketMQListener<String>{
+
+	@Override
+	public void onMessage(String message) {
+		System.out.println(message);
+	}
 	
 }
--- src/main/java/com/base/rocket/util/RocketUtil.java
+++ src/main/java/com/base/rocket/util/RocketUtil.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.Test;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
